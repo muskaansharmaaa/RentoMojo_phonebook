1a4efe3af8ec2d778c7cdbe24732d27ea2d3a844
muskaansharmaaa <54302217+muskaansharmaaa@users.noreply.github.com>
2020-05-31 19:34:53 +0530

    Create update-contact.component.js

---
+++ src/components/update-contact.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default class UpdateContact extends React.Component{
+ render(){
+   return(
+    
+    <div>
+       <p> welcome to UPDATE phonebook component!</p>
+      
+    </div>
+
+   )
+
+ }
+
+}
